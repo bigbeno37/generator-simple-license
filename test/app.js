1540368b864d73303d861462c44ff05757181bfa
bigbeno37 <bigbeno37@gmail.com>
2016-10-27 14:40:49 +1100

    Increased timeout range

--- test/app.js
+++ test/app.js
@@ -4,6 +4,8 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-simple-license:app', function () {
+  this.timeout(15000);
+
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
       .withPrompts({name: 'TESTNAME', mit: true, apache: false, GNU: false})
